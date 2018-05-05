252123dde2e081fdd0bf61ec68679b5bcc0b36c1
Ajay Koranne <akoranne@gmail.com>
2018-05-04 20:21:02 -0500

    added gradle, changes to get swagger-ui endpoint working

--- src/main/java/com/sakx/developer/demo/config/SpringFoxConfig.java
+++ src/main/java/com/sakx/developer/demo/config/SpringFoxConfig.java
@@ -1,6 +1,5 @@
 package com.sakx.developer.demo.config;
 
-import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -12,14 +11,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-
+public class SpringFoxConfig {
 	@Bean
-	public Docket productApi() {
+	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.any())
