f922e7bb37696a5f0f312986503f0ad083e4421d
Ajay Koranne <akoranne@gmail.com>
2018-05-23 13:58:51 -0400

    updated the local and cloud config in main and test packages

--- src/main/java/com/sakx/developer/demo/config/LocalConfig.java
+++ src/main/java/com/sakx/developer/demo/config/LocalConfig.java
@@ -6,4 +6,14 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("default")
 public class LocalConfig {
+
+    /**
+     * Add beans for running tests only in the `default` profile.
+     * That is, running either locally or in Concourse builds as part of CI/CD pipeline.
+     *
+     * Some examples - if testing using mock objects, implement the beans for Mock here.
+     *
+     * If you don't know, or not sure, please the class empty
+     */
+
 }
