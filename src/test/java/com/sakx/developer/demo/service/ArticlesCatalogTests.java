53ee07386cafa884e710ab6fda1c0a18b63bd662
default <akoranne@gmail.com>
2019-09-12 13:45:18 -0500

    changes related to upgrade to Spring Boot 2.1.8

--- src/test/java/com/sakx/developer/demo/service/ArticlesCatalogTests.java
+++ src/test/java/com/sakx/developer/demo/service/ArticlesCatalogTests.java
@@ -10,8 +10,8 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
